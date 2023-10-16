17cf910c3ad0f9177d1a2353ad551c812d67fada
asrytikov <asrytikov@gmail.com>
2023-10-16 21:08:15 +0400

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Comment 1
+
+        /*dfhjgdhj
+        * dfjgkh
+        * dfngkjhdfjk
+        * dfgnkjfdhkg
+        * dfgkjdhg
+        * dfgkjh*/
+
+        //System.out.println("Hello world! 1");
+        System.out.println("Hello world! 2");
+        System.out.println("Hello world! 3");
+        System.out.println("Hello world! 4");
+
+        int i = 3;
+
+        int a;
+        a=5;
+        int b = a + 3, c = 5;
+        System.out.println(c);
+        c = 100;
+        System.out.println(c);
+
+        var x = 20;
+        System.out.println(x);
+
+        final int LIMIT = 10;
+        System.out.println(LIMIT);
+
+
+    }
+}
